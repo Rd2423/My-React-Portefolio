384064451196ef17399a6449e4af7ec66d610faf
Rayan Diouf <rayandiouf@Rayans-MacBook-Pro.local>
2021-10-14 12:31:42 -0400

    I wasn't able to finish the project, I will keep working on it though

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,49 +1,18 @@
 import React, { useState } from 'react';
-import Nav from './components/Navigation'
+import Nav from './components/Nav'
 import About from './components/About'
-import Gallery from './components/Project/Gallery'
-import ContactForm from './components/Footer/Contact';
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+
 
 function App() {
-  const [choices] = useState([
-    {name: 'About Me'},
-    {name:'Portefolio'},
-    {name: 'Resume'},
-    // {name:'Contact'}
-  ]);
-  const [currentChoice, setCurrentChoice] = useState(choices[0]);
-
-  const [contactSelected, setContactSelected] = useState(false);
   return (
-    <div>
-   <Nav
-     choices={choices}
-     setCurrentChoice={setCurrentChoice}
-     currentChoice={currentChoice}
-    setContactSelected={setContactSelected}
-    contactSelected={contactSelected}
-   ></Nav>
- 
     <main>
-      {!contactSelected ? (
-        <>
-        <About currentChoice={currentChoice}></About>
-        
-        </>
-      ) : (
-        <>
-        <div>
-        <ContactForm></ContactForm>
-        </div>
-        <div>
-        <Gallery></Gallery>
-        </div>
-        </>
-      
-      )
-}
+    <Nav />
+    <About />
+    <Projects />
+
     </main>
-    </div>
   )
   }
 export default App;
