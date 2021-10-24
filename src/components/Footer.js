6540b86c64ac85d78efe62ba4e33e4ef80927ba4
Rayan Diouf <rayandiouf@Rayans-MacBook-Pro.local>
2021-10-24 17:18:51 -0400

    new version

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+    return(
+        
+        <footer>
+            <br />
+            <br />
+            <br />
+            <a href="mailto:rayandiouf0@gmail.com">Send me a email</a>
+            <br />
+            <a href="https://www.linkedin.com/in/rayan-diouf-3b3095200/">My linkedin</a>
+            <br />
+            <a href="https://github.com/Rd2423">My github</a>
+        </footer>
+    )
+}
+export default Footer;
